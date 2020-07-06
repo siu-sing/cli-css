c81f0dfe0fad0ac769c01628653925444832bd23
siu-sing <lai.siusing@gmail.com>
2020-07-06 21:27:19 +0800

    added rgb to hex

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let args = process.argv;
 
+//hex or rgb
 let input = args[2];
-let hex = args[3];
 
 let hexToRgb = (hex) => {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
@@ -12,5 +12,23 @@ let hexToRgb = (hex) => {
     } : null;
   }
 
+  let componentToHex = (c) => {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  }
+  
+  let rgbToHex = (r, g, b) => {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  }
 
-console.log(hexToRgb(hex));
+let res = null;
+switch(input){
+    case "hex":
+        let hex = args[3];
+        res = hexToRgb(hex);
+        break;
+    case "rgb":
+        res = rgbToHex(parseInt(args[3]),parseInt(args[4]),parseInt(args[5]))
+        break;
+}
+console.log(res);
